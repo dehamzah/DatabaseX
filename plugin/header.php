688ace6abc9b0679a8a764e9a1c6eb84137d7c8d
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-06-15 12:26:55 +0700

    add bootstrap

--- plugin/header.php
+++ plugin/header.php
@@ -2,3 +2,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>edXius Playground</title>
+
+<link rel="stylesheet" href="lib/css/bootstrap.min.css">
