a54c135169fe9eb87b65222580cb2fbe3e940377
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-06-15 12:42:06 +0700

    fix apex_search

--- data.php
+++ data.php
@@ -7,9 +7,35 @@ include "config/dbconnect.php";
 
 $sql = mysql_query("SELECT id, mailing_address, country, city FROM auth_userprofile ORDER BY ID ASC");
 ?>
+<script type="text/javascript">
+window.apex_search = {};
+apex_search.init = function (){
+	this.rows = document.getElementById('data').getElementsByTagName('TR');
+	this.rows_length = apex_search.rows.length;
+	this.rows_text =  [];
+	for (var i=0;i<apex_search.rows_length;i++){
+        this.rows_text[i] = (apex_search.rows[i].innerText)?apex_search.rows[i].innerText.toUpperCase():apex_search.rows[i].textContent.toUpperCase();
+	}
+	this.time = false;
+}
+apex_search.lsearch = function(){
+	this.term = document.getElementById('S').value.toUpperCase();
+	for(var i=0,row;row = this.rows[i],row_text = this.rows_text[i];i++){
+		row.style.display = ((row_text.indexOf(this.term) != -1) || this.term  === '')?'':'none';
+	}
+	this.time = false;
+}
+apex_search.search = function(e){
+    var keycode;
+    if(window.event){keycode = window.event.keyCode;}
+    else if (e){keycode = e.which;}
+    else {return false;}
+    if(keycode == 13) { apex_search.lsearch(); } else { return false; }
+}
+</script>
 </head>
 
-<body onload="">
+<body onload="apex_search.init();">
 <div class="container">
 <div class="row">
     <div class="col-lg-12">
